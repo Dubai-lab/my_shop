fb9e14ad56163e67b107554abdaea97fc0c7ce55
Dubai-lab <eg8217178@gmail.com>
2025-04-13 06:59:26 +0200

    updated my main controller

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use App\Models\User; 
 use App\Models\Product;  
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator; 
 use Illuminate\Support\Facades\Auth;
 
 class MainController extends Controller
